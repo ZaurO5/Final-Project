c3e8602c13530fc316045f32119cbd22020651e0
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2025-03-08 01:59:10 +0400

    UserList in AdminDashboard Added

---
+++ ZaireWear/Presentation/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Business.ViewModels.User;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users
+                .Select(u => new UserListVM
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email,
+                    EmailConfirmed = u.EmailConfirmed
+                })
+                .ToList();
+
+            return View(users);
+        }
+    }
+}
--- ZaireWear/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ ZaireWear/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879c5e49d1677d7ed3515649fb5269adec81e37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779a63342dd33ee784e0b81dffc4fa24ab31cc2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

10214d2a4d661cc9407c8411fce352d9be22f20d
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2025-03-22 02:45:50 +0400

    Update Before Deployment

--- ZaireWear/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ ZaireWear/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66197c74830a7bffad8b79f299e436cbdec2389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2f4f1a15bb93a5cb714c047e825e2a5cffdf11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
